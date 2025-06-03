0541baf804a5219156c96a24cb0ba81262690fd4
Dan <Daniel@yobot.bot>
2025-06-03 12:48:39 -0700

    /components/AppLayout

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { AppLayout } from './AppLayout';
+import AppLayout from './components/AppLayout';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
